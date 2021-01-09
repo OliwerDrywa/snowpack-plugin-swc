0fe19985514fe482ad4d69e3df504d16f248a08f
OliwerDrywa <oliwerdrywa@gmail.com>
2021-01-09 20:05:32 +0000

    options pass through & readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# TODO make readme
+# snowpack-plugin-swc
+
+[swc is a faster tsc and babel alternative written in rust](https://swc.rs)
+configurable with a `.swcrc` file
+
+```
+npm install --save-dev snowpack-plugin-swc
+```
+
+```js
+// snowpack.config.json
+{
+  "plugins": [
+    [
+      "snowpack-plugin-swc",
+      {
+        "input": ['.js', '.mjs', '.jsx', '.ts', '.tsx'], // (optional) specify files for swc to transform
+        transformOptions: {
+          // swc transform options
+        }
+      }
+    ]
+  ]
+}
+```
+
+#### Plugin Options
+
+| Name               | Type       | Description                                                                                                                                        |
+| :----------------- | :--------- | :------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `input`            | `string[]` | (optional) By default, swc scans & transfoms these extensions: `['.js', '.jsx', '.ts', '.tsx']`. Modify this array if you’d like to change this.   |
+| `transformOptions` | `object`   | (optional) See [https://swc.rs/docs/usage-core](https://swc.rs/docs/usage-core)                                                                    |
--- plugin.js
+++ plugin.js
@@ -2,44 +2,34 @@ const swc = require("@swc/core")
 
 module.exports = function plugin(snowpackConfig, options = {}) {
   // options validation
-  if (options) {
-    if (typeof options !== "object")
-      throw new Error(`options isn’t an object. Please see README.`)
-    if (options.input && !Array.isArray(options.input))
-      throw new Error(
-        `options.input must be an array (e.g. ['.js', '.mjs', '.jsx', '.ts', '.tsx'])`
-      )
-    if (options.input && !options.input.length)
-      throw new Error(`options.input must specify at least one filetype`)
-  }
+  if (typeof options !== "object") throw new Error(`options isn’t an object. Please see README.`)
+  if (options.input && !Array.isArray(options.input)) throw new Error(`options.input must be an array (e.g. ['.js', '.mjs', '.jsx', '.ts', '.tsx'])`)
+  if (options.input && !options.input.length) throw new Error(`options.input must specify at least one filetype`)
 
   return {
     name: "snowpack-plugin-swc",
     resolve: {
       input: options.input || [".js", ".mjs", ".jsx", ".ts", ".tsx"],
-      output: [".js"], // always export JS
+      output: [".js"],
     },
     async load({ filePath }) {
       if (!filePath) return
+      let { code, map } = await swc.transformFile(
+        filePath,
+        options.transformOptions || {}
+      )
 
-      let { code, map } = await swc.transformFile(filePath)
+      // The following is only copied from @snowpack/plugin-babel
+      // i dont know if swc plugins use process.env too
+      // TODO investgate if this is needed
 
-      if (code) {
-        // Some Babel plugins assume process.env exists, but Snowpack
-        // uses import.meta.env instead. Handle this here since it
-        // seems to be pretty common.
-        // See: https://www.pika.dev/npm/snowpack/discuss/496
+      // Some Babel plugins assume process.env exists, but Snowpack
+      // uses import.meta.env instead. Handle this here since it
+      // seems to be pretty common.
+      // See: https://www.pika.dev/npm/snowpack/discuss/496
+      if (code) code = code.replace(/process\.env/g, "import.meta.env")
 
-        // TODO investgate if this is needed
-        code = code.replace(/process\.env/g, "import.meta.env")
-      }
-
-      return {
-        ".js": {
-          code,
-          map,
-        },
-      }
+      return { ".js": { code, map } }
     },
   }
 }
